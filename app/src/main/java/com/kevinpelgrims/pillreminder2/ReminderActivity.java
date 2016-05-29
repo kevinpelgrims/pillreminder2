bfd44d3984cd39b025ffb3ebb74b06c1b65cc663
Kevin Pelgrims <kevin.pelgrims@gmail.com>
2016-05-29 19:41:47 +0200

    Save new reminder to the Firebase database

--- app/src/main/java/com/kevinpelgrims/pillreminder2/ReminderActivity.java
+++ app/src/main/java/com/kevinpelgrims/pillreminder2/ReminderActivity.java
@@ -6,6 +6,12 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.kevinpelgrims.pillreminder2.models.Reminder;
+
 import java.util.Calendar;
 import java.util.Locale;
 
@@ -18,6 +24,11 @@ public class ReminderActivity extends AppCompatActivity implements TimePickerDia
     private static final String ARG_MINUTE = "minute";
 
     @BindView(R.id.add_reminder_alarm_time) TextView alarmTimeText;
+    @BindView(R.id.add_reminder_pill_name) TextView pillNameText;
+    @BindView(R.id.add_reminder_note) TextView noteText;
+
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference database;
 
     private int selectedHour;
     private int selectedMinute;
@@ -27,6 +38,9 @@ public class ReminderActivity extends AppCompatActivity implements TimePickerDia
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminder);
         ButterKnife.bind(this);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance().getReference();
 
         if (savedInstanceState != null
                 && savedInstanceState.containsKey(ARG_HOUR)
@@ -60,6 +74,22 @@ public class ReminderActivity extends AppCompatActivity implements TimePickerDia
     @OnClick(R.id.add_reminder_alarm_time)
     void changeAlarmTime() {
         showTimePickerDialog();
+    }
+
+    @OnClick(R.id.add_reminder_save)
+    void saveReminder() {
+        final String userId = firebaseAuth.getCurrentUser().getUid();
+        final Reminder reminder = new Reminder(userId, selectedHour, selectedMinute, pillNameText.getText().toString(), noteText.getText().toString());
+
+        database.push().setValue(reminder, new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                if (databaseError == null) {
+                    // Success!
+                    finish();
+                }
+            }
+        });
     }
 
     private void showTimePickerDialog() {
