3c5207b281d4c50001a555855cfb7afbc90de1b7
Kevin Pelgrims <kevin.pelgrims@gmail.com>
2016-06-05 16:05:20 +0200

    Optimize imports

--- app/src/main/java/com/kevinpelgrims/pillreminder2/views/ReminderActivity.java
+++ app/src/main/java/com/kevinpelgrims/pillreminder2/views/ReminderActivity.java
@@ -1,8 +1,8 @@
 package com.kevinpelgrims.pillreminder2.views;
 
 import android.app.TimePickerDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
